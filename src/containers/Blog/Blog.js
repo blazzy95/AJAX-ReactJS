106db4876ea5272c5f4fc608fc9661cc7f87781a
Jitendra Selvam <jitendra3195@gmail.com>
2018-01-16 15:51:41 -0500

    Added get requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,19 +3,47 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state =  {
+        posts:[],
+        selectedPostId: null
+    }
+    
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return{
+                    ...post,
+                    author: 'Max'
+                }
+            })
+            this.setState({posts:updatedPosts});
+        });
+    }
+
+    postSelectedHandler = (id) =>{
+        this.setState({selectedPostId:id});
+    }
+
     render () {
+
+const posts = this.state.posts.map(
+    posts =>{
+        return <Post key = {posts.id} title={posts.title} author = {posts.author} clicked ={() => this.postSelectedHandler(posts.id)}/>;
+    });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id ={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />

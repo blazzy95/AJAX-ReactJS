ee83ac4fd28898045104cf81caaf75f162b3df40
Jitendra Selvam <jitendra3195@gmail.com>
2018-01-16 16:30:59 -0500

    Added Interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request =>{
+    console.log(request);
+    // we can edit requests here before sending it further
+    return request;
+}, error => {
+    console.log(error);
+    //return the error component and let the component receiving the request handle the error again
+    return Promise.reject(error); 
+});
+
+axios.interceptors.response.use(response =>{
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error); 
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
